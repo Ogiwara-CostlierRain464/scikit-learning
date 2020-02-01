a59bcec965ad5f9cff852b700f09a085e463629c
ogiwara <yushiogiwara@gmail.com>
2020-02-01 16:40:24 +0900

    add Lasso, ElasticNet

--- boston.py
+++ boston.py
@@ -2,7 +2,7 @@ from sklearn.datasets import load_boston
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LinearRegression, Lasso, ElasticNet
 from sklearn.preprocessing import StandardScaler, PolynomialFeatures
 import matplotlib.pyplot as plt
 import numpy as np
@@ -32,7 +32,10 @@ sc = StandardScaler()
 X_train_std = sc.fit_transform(X_train_pol)
 X_test_std = sc.transform(X_test_pol)
 
-model = LinearRegression()
+# model = LinearRegression()
+# model = Lasso(alpha=0.1)
+model = ElasticNet(alpha=0.1, l1_ratio=0.6)
+
 model.fit(X_train_std, y_train)
 
 y_train_pred = model.predict(X_train_std)
--- sin.py
+++ sin.py
@@ -1,5 +1,5 @@
 import numpy as np
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LinearRegression, Ridge
 from sklearn.preprocessing import PolynomialFeatures
 import matplotlib.pyplot as plt
 
@@ -10,13 +10,16 @@ X = np.random.uniform(0,4,15)[:, np.newaxis]
 # 一次元配列に, ノイズの付与
 y = np.sin(1/4 * 2 * np.pi * X).ravel() + np.random.normal(0, 0.3, 15)
 
-POLY = PolynomialFeatures(degree=3, include_bias=False)
+POLY = PolynomialFeatures(degree=6, include_bias=False)
 X_pol = POLY.fit_transform(X)
 
 model = LinearRegression()
+model2 = Ridge(alpha=0.1)
+
 
 # 同じfitが使えるようにPolynomialFeaturesを使っているだけ！
 model.fit(X_pol, y)
+model2.fit(X_pol, y)
 
 plt.figure(figsize=(8,4))
 
@@ -25,10 +28,13 @@ X_plt = np.arange(0,4,0.1)[:, None]
 y_true = np.sin(1/4 * 2 * np.pi * X_plt).ravel()
 
 y_pred = model.predict(POLY.transform(X_plt))
+y_pred2 = model2.predict(POLY.transform(X_plt))
+
 
 plt.scatter(X,y,color="blue", label="data")
 plt.plot(X_plt, y_true, color="lime", linestyle="-", label="True sin(x)")
 plt.plot(X_plt, y_pred, color="red", linestyle="-", label="LinearRegression")
+plt.plot(X_plt, y_pred2, color="blue", linestyle="--", label="Ridge")
 plt.legend(loc="upper right")
 
 plt.show()
