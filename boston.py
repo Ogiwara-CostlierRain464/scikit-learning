180a7b0c291cea689eba59b3212640a75a462671
ogiwara <yushiogiwara@gmail.com>
2020-02-01 16:05:21 +0900

    add sin

--- boston.py
+++ boston.py
@@ -3,22 +3,42 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
 from sklearn.linear_model import LinearRegression
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler, PolynomialFeatures
 import matplotlib.pyplot as plt
 import numpy as np
 
-boston = load_boston()
+np.random.normal()
 
-df = pd.DataFrame(boston.data, columns=boston.feature_names)
+df = pd.read_csv("housing.csv", header=None, sep="\s+")
 
-df["MEDV"] = boston.target
-x = df.RM.to_frame()
-y = df.MEDV
+df.columns = ["CRIM", "ZN", "INDUS", "CHAS", "NOX",
+              "RM", "AGE", "DIS", "RAD", "TAX", "PTRATIO",
+              "B", "LSTAT", "MEDV"]
 
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.3)
+# 全ての特徴量
+X = df.iloc[:, 0:13].values
+# 正解(目的変数)に住宅価格を設定
+y = df["MEDV"].values
 
-lr = LinearRegression()
+X_train, X_test, y_train, y_test = train_test_split(X, y,test_size=0.2, random_state=0)
 
-lr.fit(x_train, y_train)
+POLY = PolynomialFeatures(degree=2, include_bias=False)
 
-print(lr.predict(x_test))
+X_train_pol = POLY.fit_transform(X_train)
+X_test_pol = POLY.transform(X_test)
+
+sc = StandardScaler()
+
+X_train_std = sc.fit_transform(X_train_pol)
+X_test_std = sc.transform(X_test_pol)
+
+model = LinearRegression()
+model.fit(X_train_std, y_train)
+
+y_train_pred = model.predict(X_train_std)
+y_test_pred = model.predict(X_test_std)
+
+print("MSE train: {0}, test: {1}".format(
+    mean_squared_error(y_train, y_train_pred),
+    mean_squared_error(y_test, y_test_pred)
+))
---
+++ sin.py
@@ -0,0 +1,34 @@
+import numpy as np
+from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
+import matplotlib.pyplot as plt
+
+np.random.seed(seed=8)
+
+# 二次元配列に
+X = np.random.uniform(0,4,15)[:, np.newaxis]
+# 一次元配列に, ノイズの付与
+y = np.sin(1/4 * 2 * np.pi * X).ravel() + np.random.normal(0, 0.3, 15)
+
+POLY = PolynomialFeatures(degree=3, include_bias=False)
+X_pol = POLY.fit_transform(X)
+
+model = LinearRegression()
+
+# 同じfitが使えるようにPolynomialFeaturesを使っているだけ！
+model.fit(X_pol, y)
+
+plt.figure(figsize=(8,4))
+
+X_plt = np.arange(0,4,0.1)[:, None]
+# 正解
+y_true = np.sin(1/4 * 2 * np.pi * X_plt).ravel()
+
+y_pred = model.predict(POLY.transform(X_plt))
+
+plt.scatter(X,y,color="blue", label="data")
+plt.plot(X_plt, y_true, color="lime", linestyle="-", label="True sin(x)")
+plt.plot(X_plt, y_pred, color="red", linestyle="-", label="LinearRegression")
+plt.legend(loc="upper right")
+
+plt.show()
