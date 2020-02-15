35e4084cf1798fe89ae91f4208264584599855c2
ogiwara <yushiogiwara@gmail.com>
2020-02-15 18:25:09 +0900

    add merge

--- algorithm_introduction/chapter2.py
+++ algorithm_introduction/chapter2.py
@@ -22,33 +22,34 @@ def linear_search(a, v):
     return None
 
 
-def merge(a, p, q, r):
+def merge(A, p, q, r):
     n1 = q - p + 1
     n2 = r - q
-    l = AlgorithmArray.empty(n1 + 1)
-    r = AlgorithmArray.empty(n2 + 1)
+    L = AlgorithmArray.empty(n1 + 1)
+    R = AlgorithmArray.empty(n2 + 1)
     for i in count(1, n1):
-        l[i] = a[p * i - 1]
+        L[i] = A[p + i - 1]
     for j in count(1, n2):
-        r[j] = a[q + j]
-    l[n1 + 1] = INF
-    r[n2 + 1] = INF
+        R[j] = A[q + j]
+    L[n1 + 1] = INF
+    R[n2 + 1] = INF
     i = 1
     j = 1
     for k in count(p, r):
-        if l[i] <= r[j]:
-            a[k] = l[i]
+        if L[i] <= R[j]:
+            A[k] = L[i]
             i = i + 1
-        elif a[k] == r[j]:
+        else:
+            A[k] = R[j]
             j = j + 1
 
 
-def marge_sort(a, p, r):
+def marge_sort(A, p, r):
     if p < r:
         q = floor((p + r) / 2)
-        marge_sort(a, p, q)
-        marge_sort(a, q + 1, r)
-        merge(a, p, q, r)
+        marge_sort(A, p, q)
+        marge_sort(A, q + 1, r)
+        merge(A, p, q, r)
 
 
 arr = AlgorithmArray([5, 2, 4, 6, 1, 3])
