67b15b380b0d9d914145d79160b496cc12e55de6
ogiwara <yushiogiwara@gmail.com>
2020-02-16 17:40:36 +0900

    add separate_quantum

--- algorithm_introduction/chapter4.py
+++ algorithm_introduction/chapter4.py
@@ -1,5 +1,6 @@
 from algorithm_introduction.helper import *
 from math import *
+import numpy as np
 
 
 def find_max_crossing_subarray(A, low, mid, high):
@@ -57,9 +58,60 @@ def full_search(A):
     return max_diverge
 
 
-# arr = AlgorithmArray([13, -3, -23, 18, 20, -7, 12, -5, -22, 15, -4, 7])
+def linear_search(A):
+    m = -INF
+    low_m = high_m = None
+    m_r = 0
+    low_r = 1
+    for i in count(1, A.length):
+        m_r += A[i]
+        if m_r > m:
+            low_m = low_r
+            high_m = i
+            m = m_r
+        if m_r < 0:
+            m_r = 0
+            low_r = i + 1
+
+    return low_m, high_m, m
+
+
+def strassen(A: np.ndarray, B: np.ndarray):
+    s1 = B[0, 1] - B[1, 1]
+    s2 = A[0, 0] + A[0, 1]
+    s3 = A[1, 0] + A[1, 1]
+    s4 = B[1, 0] - B[0, 0]
+    s5 = A[0, 0] + A[1, 1]
+    s6 = B[0, 0] + B[1, 1]
+
+
+def separate_quantum(A: np.ndarray):
+    x = A.shape[0]
+    y = A.shape[1]
+    assert x == y
+    n = int(x / 2)
+    A11 = A[0:n, 0:n]
+    A12 = A[0:n, n:]
+    A21 = A[n:, 0:n]
+    A22 = A[n:, n:]
+    return A11, A12, A21, A22
+
+
+arr = AlgorithmArray([13, -3, -23, 18, 20, -7, 12, -5, -22, 15, -4, 7])
 # arr = AlgorithmArray([-5, -9, -78, -4, -3, -3])
-arr = AlgorithmArray([13, 3, 5, 6, 8, 3])
+# arr = AlgorithmArray([13, 3, 5, 6, 8, 3])
+
+matrix = np.array(
+    [
+        [1, 2, 3, 4],
+        [5, 6, 7, 8],
+        [9, 10, 11, 12],
+        [13, 14, 15, 16]
+    ]
+)
+
+print(separate_quantum(matrix))
 
 # print(find_maximum_subarray(arr, 1, arr.length))
-print(full_search(arr))
+# print(full_search(arr))
+# print(linear_search(arr))
