bdcf92c14291358a873d261d43386015a906aa6f
ogiwara <yushiogiwara@gmail.com>
2020-02-25 17:33:17 +0900

    add NN sample
    cf. ゼロからはじめるDeep Learning

---
+++ neural_network/main.py
@@ -0,0 +1,131 @@
+import numpy as np
+
+
+# from 17:00
+def step_function(x):
+    return np.array(x > 0, dtype=np.int)
+
+
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
+
+
+def relu(x):
+    return np.maximum(0, x)
+
+
+def identity_function(x):
+    return x
+
+
+def softmax(a):
+    c = np.max(a)
+    exp_a = np.exp(a - c)  # support Overflow
+    sum_exp_a = np.sum(exp_a)
+    y = exp_a / sum_exp_a
+    return y
+
+
+def mean_squared_error(y, t):
+    return 0.5 * np.sum((y - t) ** 2)
+
+
+def cross_entropy_error(y, t):
+    if y.ndim == 1:
+        t = t.reshape(1, t.size)
+        y = y.reshape(1, y.size)
+
+    if t.size == y.size:
+        t = t.argmax(axis=1)
+
+    batch_size = y.shape[0]
+    return -np.sum(np.log(y[np.arange(batch_size), t] + 1e-7)) / batch_size
+
+
+def _numerical_gradient_no_batch(f, x):
+    h = 1e-4  # 0.0001
+    grad = np.zeros_like(x)
+
+    for idx in range(x.size):
+        print(idx)
+        tmp_val = x[idx]
+        x[idx] = float(tmp_val) + h
+        fxh1 = f(x)  # f(x+h)
+
+        x[idx] = tmp_val - h
+        fxh2 = f(x)  # f(x-h)
+        grad[idx] = (fxh1 - fxh2) / (2 * h)
+
+        x[idx] = tmp_val  # 値を元に戻す
+
+    return grad
+
+
+def numerical_gradient(f, X):
+    if X.ndim == 1:
+        return _numerical_gradient_no_batch(f, X)
+    else:
+        grad = np.zeros_like(X)
+
+        for idx, x in enumerate(X):
+            grad[idx] = _numerical_gradient_no_batch(f, x)
+
+        return grad
+
+
+def gradient_descent(f, init_x, lr=0.01, step_num=100):
+    x = init_x
+
+    for i in range(step_num):
+        grad = numerical_gradient(f, x)
+        x -= lr * grad
+
+    return x
+
+
+class NeuralNet:
+    def __init__(self, input_size, hidden_size, output_size,
+                 weight_init_std=0.01):
+        self.params = {}
+        self.params['W1'] = weight_init_std * np.random.randn(input_size, hidden_size)
+        self.params['b1'] = np.zeros(hidden_size)
+        self.params['W2'] = weight_init_std * np.random.randn(hidden_size, output_size)
+        self.params['b2'] = np.zeros(output_size)
+
+    def predict(self, x):
+        W1, W2 = self.params["W1"], self.params["W2"]
+        b1, b2 = self.params["b1"], self.params["b2"]
+        a1 = np.dot(x, W1) + b1
+        z1 = sigmoid(a1)
+        a2 = np.dot(z1, W2) + b2
+        y = sigmoid(a2)
+        return y
+
+    def loss(self, x, t):
+        y = self.predict(x)
+
+        return cross_entropy_error(y, t)
+
+    def accuracy(self, x, t):
+        y = self.predict(x)
+        y = np.argmax(y, axis=1)
+        t = np.argmax(t, axis=1)
+
+        accuracy = np.sum(y == t) / float(x.shape[0])
+        return accuracy
+
+    def numerical_gradient(self, x, t):
+        loss_W = lambda W: self.loss(x, t)
+
+        grads = {}
+        grads['W1'] = numerical_gradient(loss_W, self.params['W1'])
+        grads['b1'] = numerical_gradient(loss_W, self.params['b1'])
+        grads['W2'] = numerical_gradient(loss_W, self.params['W2'])
+        grads['b2'] = numerical_gradient(loss_W, self.params['b2'])
+
+        return grads
+
+
+
+
+
---
+++ neural_network/play.py
@@ -0,0 +1,35 @@
+from sklearn.datasets import fetch_openml
+from sklearn.model_selection import train_test_split
+from neural_network.main import *
+import numpy as np
+import matplotlib.pyplot as plt
+from neural_network.mnist import load_mnist
+
+(X_train, t_train), (X_test, t_test) = load_mnist(normalize=True, one_hot_label=True)
+
+train_loss_list = []
+
+iter_num = 10000
+train_size = X_train.shape[0]
+batch_size = 100
+learning_rate = 0.1
+
+network = NeuralNet(input_size=784, hidden_size=50, output_size=10)
+
+for i in range(iter_num):
+    print("Iter {0}".format(i))
+    batch_mask = np.random.choice(train_size, batch_size)
+    x_batch = X_train[batch_mask]
+    t_batch = t_train[batch_mask]
+
+    grad = network.numerical_gradient(x_batch, t_batch)
+
+    for key in ("W1", "b1", "W2", "b2"):
+        network.params[key] -= learning_rate * grad[key]
+
+    loss = network.loss(x_batch, t_batch)
+    train_loss_list.append(loss)
+
+
+plt.scatter(train_loss_list)
+plt.show()
