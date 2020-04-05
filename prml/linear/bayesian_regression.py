b7ea3fb36efb9915761084796805e45f09b7423b
ogiwara <yushiogiwara@gmail.com>
2020-04-05 09:37:59 +0900

    update bayesian regression

--- prml/linear/bayesian_regression.py
+++ prml/linear/bayesian_regression.py
@@ -18,32 +18,26 @@ class BayesianRegression(Regression):
         self.beta = beta
         self.w_mean = None
         self.w_precision = None
-        self.w_cov = None
 
     def fit(self, X: np.ndarray, t: np.ndarray):
         """
         cf. (3.50)
         """
+
         mean_prev, precision_prev = self._get_prior(np.size(X, 1))
         w_precision = precision_prev + self.beta * X.T @ X
-
-        # w_mean = np.linalg.inv(w_precision) @ (precision_prev @ mean_prev + self.beta * X.T @ t)
-        w_mean = np.linalg.solve(
-            w_precision,
-            precision_prev @ mean_prev + self.beta * X.T @ t
-        )
+        w_mean = np.linalg.inv(w_precision) @ (precision_prev @ mean_prev + self.beta * X.T @ t)
 
         self.w_mean = w_mean
         self.w_precision = w_precision
-        # why not solve inv only at predict step?
-        self.w_cov = np.linalg.inv(self.w_precision)
 
     def predict(self, X: np.ndarray):
         """
         cf. (3.58)
         """
         y = X @ self.w_mean
-        y_var = 1 / self.beta + np.sum(X @ self.w_cov * X, axis=1)
+        w_cov = np.linalg.inv(self.w_precision)
+        y_var = 1 / self.beta + np.sum(X @ w_cov * X, axis=1)
         y_std = np.sqrt(y_var)
         return y, y_std
 
